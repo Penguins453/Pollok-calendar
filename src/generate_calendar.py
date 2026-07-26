b0766f3cbc1d5f396abb942ef65f827e06a25fe8
Penguins453 <308014186+Penguins453@users.noreply.github.com>
2026-07-26 18:10:55 +0100

    Represent fixtures with a dataclass

--- src/generate_calendar.py
+++ src/generate_calendar.py
@@ -2,11 +2,20 @@
 
 """Pollok FC Calendar Generator."""
 
+from dataclasses import dataclass
+
 import requests
 from bs4 import BeautifulSoup
 
 FIXTURES_URL = "https://www.pollokfc.com/calendar/fixtures-and-results-2026-27/"
 
+@dataclass
+class Fixture:
+    date: str
+    match: str
+    result: str
+    competition: str
+    ground: str
 
 def main():
     print("⚽ Pollok Calendar Generator")
@@ -40,20 +49,19 @@ def main():
     for row in rows[1:]:
         cells = row.find_all("td")
 
-        fixture = {
-            "date": cells[0]["content"],
-            "match": cells[1].get_text(strip=True),
-            "result": cells[2].get_text(strip=True),
-            "competition": cells[3].get_text(strip=True),
-            "ground": cells[4].get_text(strip=True),
-        }
+        fixture = Fixture(
+            date=cells[0]["content"],
+            match=cells[1].get_text(strip=True),
+            result=cells[2].get_text(strip=True),
+            competition=cells[3].get_text(strip=True),
+            ground=cells[4].get_text(strip=True),
+        )
 
         fixtures.append(fixture)
-
     print(f"Extracted {len(fixtures)} fixtures.\n")
 
     for fixture in fixtures [:5]:
         print(fixture)
         
 if __name__ == "__main__":
-        main()
+    main()
