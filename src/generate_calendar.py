b964193b21b75dcc6044d979cbae3104248836a9
Penguins453 <308014186+Penguins453@users.noreply.github.com>
2026-07-26 17:59:33 +0100

    Extract all fixtures from website

--- src/generate_calendar.py
+++ src/generate_calendar.py
@@ -35,19 +35,25 @@ def main():
 
     print(f"Found {len(rows)-1} fixtures\n")
 
-    first_fixture = rows[1]
+    fixtures = []
 
-    cells = first_fixture.find_all("td")
+    for row in rows[1:]:
+        cells = row.find_all("td")
 
-    fixture = {
-        "date": cells[0]["content"],
-        "match": cells[1].get_text(strip=True),
-        "result": cells[2].get_text(strip=True),
-        "competition": cells[3].get_text(strip=True),
-        "ground": cells[4].get_text(strip=True),
-    }
+        fixture = {
+            "date": cells[0]["content"],
+            "match": cells[1].get_text(strip=True),
+            "result": cells[2].get_text(strip=True),
+            "competition": cells[3].get_text(strip=True),
+            "ground": cells[4].get_text(strip=True),
+        }
 
-    print(fixture)
+        fixtures.append(fixture)
 
+    print(f"Extracted {len(fixtures)} fixtures.\n")
+
+    for fixture in fixtures [:5]:
+        print(fixture)
+        
 if __name__ == "__main__":
         main()
