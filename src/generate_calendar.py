707b582c8497353d88d01c15e2c6a5eb594b21e9
Penguins453 <308014186+Penguins453@users.noreply.github.com>
2026-09-12 15:57:43 +0100

    Add home and away fixture details

--- src/generate_calendar.py
+++ src/generate_calendar.py
@@ -3,6 +3,7 @@
 """Pollok FC Calendar Generator."""
 
 from dataclasses import dataclass
+from datetime import datetime
 
 import requests
 from bs4 import BeautifulSoup
@@ -11,16 +12,15 @@ FIXTURES_URL = "https://www.pollokfc.com/calendar/fixtures-and-results-2026-27/"
 
 @dataclass
 class Fixture:
-    date: str
+    date: datetime
     match: str
+    opponent: str
+    home: bool
     result: str
     competition: str
     ground: str
-
-def main():
-    print("⚽ Pollok Calendar Generator")
-    print("=" * 40)
-
+    
+def download_page():
     headers = {
         "User-Agent": (
             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
@@ -36,8 +36,9 @@ def main():
     )
     response.raise_for_status()
 
-    soup = BeautifulSoup(response.text, "html.parser")
+    return BeautifulSoup(response.text, "html.parser")
 
+def extract_fixtures(soup):
     table = soup.find_all("table")[0]
 
     rows = table.find_all("tr")
@@ -49,19 +50,43 @@ def main():
     for row in rows[1:]:
         cells = row.find_all("td")
 
+        match = cells[1].get_text(strip=True)
+
+        if match.startswith("Pollok v "):
+            home = True
+            opponent = match.removeprefix("Pollok v ")
+        elif match.endswith(" v Pollok"):
+            home = False
+            opponent = match.removesuffix(" v Pollok")
+        else:
+            home = False
+            opponent = match
+
         fixture = Fixture(
-            date=cells[0]["content"],
-            match=cells[1].get_text(strip=True),
+            date=datetime.fromisoformat(cells[0]["content"]),
+            match=match,
+            opponent=opponent,
+            home=home,
             result=cells[2].get_text(strip=True),
             competition=cells[3].get_text(strip=True),
             ground=cells[4].get_text(strip=True),
         )
-
         fixtures.append(fixture)
+
+    return fixtures
+
+def main():
+    print("⚽ Pollok Calendar Generator")
+    print("=" * 40)
+
+    soup = download_page()
+
+    fixtures = extract_fixtures(soup)
+
     print(f"Extracted {len(fixtures)} fixtures.\n")
 
-    for fixture in fixtures [:5]:
+    for fixture in fixtures[:5]:
         print(fixture)
-        
+
 if __name__ == "__main__":
     main()
